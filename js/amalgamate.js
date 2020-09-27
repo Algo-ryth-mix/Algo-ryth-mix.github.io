d7e4d8385d7ff922142eeeb851023aeae159af43
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-09-27 14:28:03 +0200

    added blog site, with comment function

--- js/amalgamate.js
+++ js/amalgamate.js
@@ -7,11 +7,43 @@ function loadFile(filename) {
   });
 }
 
-async function amalgamate(manifest) {
+function findGetParameter(parameterName) {
+  var result = null,
+      tmp = [];
+  location.search
+      .substr(1)
+      .split("&")
+      .forEach(function (item) {
+        tmp = item.split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+      });
+  return result;
+}
+
+function make_manifest(content)
+{
+  return "#autogen-manifest\neaster-egg\nnav\n" + content
+}
+
+function make_blog_entry(content,location)
+{
+  if(content.startsWith("<!-- Blog -->")){
+    console.log("find:" + content.search("<!-- Jumplink -->").toString());
+    const title = content.substr(content.search("<!--Title")+10,content.search("Title-->") - content.search("<!--Title") - 11); 
+    return content.substr(0,content.search("<!-- Jumplink -->")) + `<br><a href='article.html?x=${location}&t=${title}'> Keep Reading </a></article>`
+  }
+  else return content
+}
+
+async function amalgamate(manifest,blogify = false,manifest_is_contents = false) {
   console.log("amalgamate loaded!");
+  var contents = "";
 
   //get entries to load
-  const contents  = await loadFile("./sub/"+manifest+".txt").then(response => response.contents.then(c => c));
+  if(!manifest_is_contents)
+    contents  = await loadFile("./sub/"+manifest+".txt").then(response => response.contents.then(c => c));
+  else
+    contents = manifest
 
   const root = document.getElementById("root");
 
@@ -24,6 +56,10 @@ async function amalgamate(manifest) {
 
     line = line.replace(".","");
     const content = await loadFile("./sub/"+line+".html").then(response => response.contents.then(c => c));
+    if(blogify)
+    {
+      return make_blog_entry(content,line);
+    }
     //root.innerHTML+= content;
     return content;
   })
